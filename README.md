9433427371a1893dbd5cf588c10ac276a0f8a9c5
Sironix <raichirup@gmail.com>
2022-01-14 23:21:21 -0300

    new tower type!

--- README.md
+++
@@ -1,2 +0,0 @@
-# Godot_RegularPolygon2D
-RegularPolygon2D Node for Godot game Engine. Quickly create polygons with equal sides and angles.
--- Scenes/Towers/Turrets.gd
+++ Scenes/Towers/Turrets.gd
@@ -3,22 +3,42 @@ extends Node2D
 var enemy_array = []
 var enemy
 
+var ready = true
 var built = false
-var self_range
+
 var type
 var category
-var ready = true
+var projectile
+var self_range
+var shotspeed
+var damage
+var rate
+var pierce
+var muzzle_path
 
-
+func setup(build_type,location):
+	position = location
+	built = true
+	type = build_type
+	category = GameData.tower_data[type]["category"]
+	self_range = GameData.tower_data[type]["range"]
+	damage = GameData.tower_data[type]["damage"]
+	rate = GameData.tower_data[type]["rate"]
+	if category != "instant":
+		projectile = GameData.tower_data[type]["projectile"]
+		shotspeed = GameData.tower_data[type]["shotspeed"]
+		pierce = GameData.tower_data[type]["pierce"]
 func _ready():
 	if built:
-		self_range = GameData.tower_data[type]["range"]
 		self.get_node("Range/CollisionShape2D").scale = Vector2(	(self_range + 0.5), (self_range + 0.5) )
 
 func _physics_process(delta):
 	if enemy_array.size() > 0 and built:
 		select_enemy()
-		if not get_node("AnimationPlayer").is_playing():
+		if category == "instant":
+			if not get_node("AnimationPlayer").is_playing():
+				turn()
+		else:
 			turn()
 		if ready:
 			fire()
@@ -44,15 +64,20 @@ func fire():
 	elif category == "projectile":
 		fire_projectile()
 	
-	enemy.on_hit(GameData.tower_data[type]["damage"])
-	yield(get_tree().create_timer(GameData.tower_data[type]["rate"]), "timeout")
+	
+	yield(get_tree().create_timer(rate), "timeout")
 	ready = true
 
 func fire_instant():
 	get_node("AnimationPlayer").play("Fire")
-
+	enemy.on_hit(damage)
+	
 func fire_projectile():
-	pass
+	var new_shot = load("res://Scenes/Towers/" + projectile + ".tscn").instance()
+	var muzzle_position = self.position + Vector2(32,32) + muzzle_path.position.rotated(self.get_node("Turret").rotation)
+	new_shot.setup(shotspeed,self.get_node("Turret").rotation,self_range,muzzle_position,damage,pierce)
+	GameData.GameScene_Path.get_node("Projectiles").add_child(new_shot, true)
+
 
 func _on_Range_body_entered(body):
 	enemy_array.append(body.get_parent())
